b26b767c20dfe4bcdd6ea0b970ba2b9bf03e8e66
Shravan0x <shravanparthe19@gmail.com>
2025-01-19 22:05:31 +0530

    Datatypes in JAVA

---
+++ Datatypes.java
@@ -0,0 +1,74 @@
+public class Datatypes {
+
+    public static void main(String[] args) {
+        int a = 9;
+        byte by = 127;
+        short sh = 558;
+        long l = 58541l;  // whenever you mention a long value always put l at the end
+         
+
+        float f = 5.208f; // whenever you mention a long value always put f at the end
+        double d = 20.56;
+
+
+        char c = 's'; // all the character are mentioned in single quotes ' ';
+        boolean b = true; // boolean returns only true or false
+        System.out.println(a);
+        System.out.println(by);
+        System.out.println(sh);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+
+    }
+    
+}
+/*
+ 
+                                                           DATATYYPES IN JAVA 
+
+
+      
+      The datatype is a type of data represented by a variable
+      * java datatypes are case sensitive
+      * there are two types of datatypes :
+      1. PRIMITIVE DATA TYPES
+       it is further classified into:
+       1.Numeric and 2. Non-numeric
+       
+       1.Numeric 
+       it includes datatypes as follows
+       * int    (integer)
+       * float  (floating point characters)
+       
+
+       2. Non-numeric 
+       it incluses datatypes as follows:
+       * Char     (character values)
+       * Boolean  (true or false)
+
+    Non primitive datatypes includes 
+    1. classes
+    2. Arrays
+    3. interface
+
+
+    Datatypes and range
+
+    datatype        |           size    |     range
+                 
+   1. byte                      1         -128 to 127
+   2. boolean                   1         true or false
+   3. char                      2         A-Z , a-z , 0-9, etc;
+   4. short                     2         -32768 to 32767
+   5. Int                       4         (about) -2 million to 2 mo     
+   6. long                      8         (about)  -10E18 to 10E18
+   7. float                     4         -3.4E38 to 3.4E18
+   8. double                    8         -1.7E308 to 1.7E308
+
+
+    
+   
+ */
