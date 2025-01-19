d8fc5ff281d8fdaa4d3b7572472fa641d7fbf50e
Shravan Sandeep Parthe <shravanparthe19@gmail.com>
2025-01-19 21:55:25 +0530

    Variables.java

---
+++ Variables.java
@@ -0,0 +1,102 @@
+ // simple program to simplify the use of datatypes and use of variables 
+
+
+
+public class Variables {
+   public static void main(String[] args) {
+    int a = 2;  // datatype is integer , data or value ie 2 is stored in variable a
+    int b = 3;  // datatype is integer , data or value ie 3 is stored in variable b
+    int sum = a+b;  // the sum is the variable that stores the value of the sum of a+b
+    System.out.println("The sum of the numbers is:"); // use of System.out.println to print on a new line 
+    System.out.print(sum);   // use of System.out.print to print the sum 
+   }
+}
+
+
+
+// java is a strongly typed language : 
+
+/*
+ Variables in java are used to store data values. A variable is a name given to a memory location that stores a value.
+ Types of variables :
+ 1. Local variables
+ 2. Instance variables
+ 3. Static variabels
+ 4. Parameter variables 
+
+ we will understand types of variables in further modules
+
+ how to declare a variable
+ "to declare a variable in java you need to specify the data type and the variable name"
+ example :
+      int myInteger;
+      double myDouble;
+      boolean myBoolean;
+
+
+      variable initialization:
+      you can initialize or assign a value to a variable when you declare it.
+    
+      example :
+      int myInteger = 10;
+      double myDouble = 22.0;
+      boolean myBoolean  = true;
+
+
+                                                           DATATYYPES IN JAVA 
+
+
+      
+      The datatype is a type of data represented by a variable
+      * java datatypes are case sensitive
+      * there are two types of datatypes :
+      1. PRIMITIVE DATA TYPES
+       it is further classified into:
+       1.Numeric and 2. Non-numeric
+       
+       1.Numeric 
+       it includes datatypes as follows
+       * int    (integer)
+       * float  (floating point characters)
+       
+
+       2. Non-numeric 
+       it incluses datatypes as follows:
+       * Char     (character values)
+       * Boolean  (true or false)
+
+    Non primitive datatypes includes 
+    1. classes
+    2. Arrays
+    3. interface
+
+
+    Datatypes and range
+
+    datatype        |           size    |     range
+                 
+   1. byte                      1         -128 to 127
+   2. boolean                   1         true or false
+   3. char                      2         A-Z , a-z , 0-9, etc;
+   4. short                     2         -32768 to 32767
+   5. Int                       4         (about) -2 million to 2 mo     
+   6. long                      8         (about)  -10E18 to 10E18
+   7. float                     4         -3.4E38 to 3.4E18
+   8. double                    8         -1.7E308 to 1.7E308
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
